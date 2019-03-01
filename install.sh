a1f325d77f90005f944f8fe39211544e1fbb7b4d
codekoch <olaf.koch@aeg-kaarst.org>
2019-03-01 14:02:09 +0100

    RaspberryPiVersion

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "install and configure everything..."
+sudo update-rc.d -f randomWifi remove
 echo "copying every script to the right place..."
 sudo cp -R ./install/* /
 echo "done! A restart is necessary!"
